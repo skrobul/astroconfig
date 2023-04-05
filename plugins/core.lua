d822ffc7f49a2e6d5736b03ef1d4cca835e6afa4
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-04-06 00:17:34 +0200

    reorganise the start page to include projects

--- plugins/core.lua
+++ plugins/core.lua
@@ -12,6 +12,15 @@ return {
         "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
         "    ██   ████   ████   ██ ██      ██",
       }
+      local button = require("astronvim.utils").alpha_button
+      opts.section.buttons.val = {
+        button("LDR n", "  New File  "),
+        button("LDR f p", "  Projects  "),
+        button("LDR f f", "  Find File  "),
+        button("LDR f o", "  Recents  "),
+        button("LDR f w", "  Find Word  "),
+        button("LDR f '", "  Bookmarks  "),
+        button("LDR S l", "  Last Session  "), }
       return opts
     end,
   },
