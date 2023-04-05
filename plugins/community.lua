30eb79032cba423efca185b36278377f9cd74411
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-04-05 23:13:14 +0200

    astrovim: enable project plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.motion.leap-nvim" }
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.project.project-nvim" },
 }
