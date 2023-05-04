7cd196d8d2a7375699503f3d0f5440ea6c95123c
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-05-04 13:03:25 +0100

    fix the highlighting for leap.nvim

---
+++ plugins/astrotheme.lua
@@ -0,0 +1,13 @@
+return {
+  "AstroNvim/astrotheme",
+  opts = {
+    plugins = {
+      ["dashboard-nvim"] = true
+    },
+    highlights = {
+      global = {
+        ["Cursor"] = { fg = "#cc626a" }
+      }
+    }
+  },
+}
