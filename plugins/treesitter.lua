07f5bf684172fd2966eb9a05475125adf96f0367
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-06-14 15:22:52 +0100

    configure nvim-treesitter-textobject

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,4 +3,11 @@ return {
   opts = {
     ensure_installed = { "lua", "ruby", "python", "yaml" },
   },
+  dependencies = {
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      after = 'nvim-treesitter',
+      -- config = function() require('settings.treesitter') end,
+    },
+  },
 }
