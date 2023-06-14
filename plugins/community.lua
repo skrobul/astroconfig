e10b4cd77857d44c071af74aae70b998b97ef063
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-06-14 15:24:03 +0100

    disable project, cinnamon, yaml pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,15 +7,12 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
-  { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.syntax.vim-easy-align" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.colorscheme.catppuccin" }
 }
