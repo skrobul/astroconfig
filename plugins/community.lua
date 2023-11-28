330b01c1fbeac764f7e714db1469a8e3d43a49da
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-11-28 19:44:17 +0000

    community: switch to the python-ruff

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,7 +10,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.syntax.vim-easy-align" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
