094c3bc2de1b38089a207e5adb4d8b3d13b8b721
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-12-11 23:18:07 +0000

    switch to prettierd

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -11,7 +11,7 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettierd,
       null_ls.builtins.formatting.rubocop,
       null_ls.builtins.formatting.sql_formatter,
       null_ls.builtins.formatting.jq,
