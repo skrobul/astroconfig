2804f4b58067fa4c04ee21e4f42b37d5376dd9cb
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-12-20 20:34:27 +0000

    more fixes to whitespaces

--- plugins/whitespace.lua
+++ plugins/whitespace.lua
@@ -2,29 +2,30 @@ return {
   {
     "johnfrankmorgan/whitespace.nvim",
     opts = {
-            -- configuration options and their defaults
+      -- configuration options and their defaults
 
-            -- `highlight` configures which highlight is used to display
-            -- trailing whitespace
-            highlight = 'DiffDelete',
+      -- `highlight` configures which highlight is used to display
+      -- trailing whitespace
+      highlight = 'DiffDelete',
 
-            -- `ignored_filetypes` configures which filetypes to ignore when
-            -- displaying trailing whitespace
-            ignored_filetypes = {
-              'TelescopePrompt',
-              'Trouble',
-              'help',
-              'mason',
-              'lazy',
-              'WhichKey',
-              'alpha',
-              'gitcommit',
-              'toggleterm',
-              'dap-repl'
-            },
+      -- `ignored_filetypes` configures which filetypes to ignore when
+      -- displaying trailing whitespace
+      ignored_filetypes = {
+        'TelescopePrompt',
+        'Trouble',
+        'help',
+        'mason',
+        'lazy',
+        'WhichKey',
+        'alpha',
+        'gitcommit',
+        'toggleterm',
+        'dap-repl',
+        'telescopeprompt',
+      },
 
-            -- `ignore_terminal` configures whether to ignore terminal buffers
-            ignore_terminal = true,
+      -- `ignore_terminal` configures whether to ignore terminal buffers
+      ignore_terminal = true,
     },
     config = function(_, opts)
       require('whitespace-nvim').setup(opts)
