0ebe55165d1815c306a2bf6854f9bd022a8449ac
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-02-21 21:27:02 +0000

    diffview: unmap leader>c to avoid conflicts

--- plugins/diffview.lua
+++ plugins/diffview.lua
@@ -3,5 +3,8 @@ return {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewFileHistory", "DiffviewOpen", "DiffviewFocusFiles", "DiffviewLog" },
     event = "User AstroGitFile",
+    keys = {
+      { "<leader>c", false, buffer=true, desc="Conflict resolution" },
+    }
   }
 }
